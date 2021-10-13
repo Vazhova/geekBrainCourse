a58294b9c6c7110d8668b72ee07eeac933d4e4f0
Yuliya Nazarova <yuliya.nazarova@wheely.com>
2021-10-13 17:48:28 +0300

    Homework 2

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -2,8 +2,64 @@ package Lesson2;
 
 public class Lesson2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("I am Lesson2 branch");
+        checkSum(10, 3);
+        posOrNegWithPrint(-9);
+        posOrNegReturn(9);
+        printString("Privet", 8);
+        leapYearOrNot(2021);
+//      leapYearOrNottest(400);
     }
+
+    public static boolean checkSum(int a, int b) {
+        if (10 >= (a + b) && (a + b) <= 20) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void posOrNegWithPrint(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное число");
+        } else
+            System.out.println("Отрицательное число");
+    }
+
+    public static boolean posOrNegReturn(int a) {
+        if (a < 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void printString(String a, int b) {
+        for (int i = 1; i <= b; i++) {
+            System.out.println(a);
+        }
+    }
+
+    public static boolean leapYearOrNot(int c) {
+        if (c % 400 == 0) {
+            return true;
+        } else if ((c % 100 == 0)) {
+            return false;
+        } else if ((c % 4 == 0) || (c % 400 == 0))  {
+            return true;
+        } else
+            return false;
+    }
+
+// Проверка 5 задания
+//  public static void leapYearOrNottest(int c) {
+//     if (c % 400 == 0) {
+//          System.out.println("Високосный");
+//     } else if ((c % 100 == 0)) {
+//          System.out.println("Невисокосный");
+//     } else if ((c % 4 == 0) || (c % 400 == 0))  {
+//          System.out.println("Високосный");
+//     } else
+//          System.out.println("Невисокосный");
+//     }
 }
+
